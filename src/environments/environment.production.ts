be4dda2783a9403767c5effc4490185830fa1fec
Mathieu <mathieu.eylenbosch@gmail.com>
2026-02-14 13:46:45 +0100

    Changed backend URI

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
     GOOGLE_CLIENT_ID: "570259636301-bc4r0ai4pdqt95uacjmqc3qr5srial4a.apps.googleusercontent.com",
     // ENDPOINT_URI: "https://pmt-a2hdh0hra6apg3dq.francecentral-01.azurewebsites.net",
-    ENDPOINT_URI: "https://eylenboschmathieu.runasp.net/"
+    ENDPOINT_URI: "https://eylenboschmathieu.runasp.net"
 };
